afe14f3ac7c21b74e0c2a8b246f088c17b83a6d5
BuildTools <arthurpisakhov1@gmail.com>
2019-09-01 01:15:31 -0400

    starting CSS

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Operators from './components/ButtonComponents/OperatorButtons/Operators';
 import Specials from './components/ButtonComponents/SpecialButtons/Specials';
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Display from './components/DisplayComponents/Display';
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -20,9 +21,10 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display />
+        <Specials />
         <Numbers />
         <Operators />
-        <Specials />
       </div>
     </div>
   );
